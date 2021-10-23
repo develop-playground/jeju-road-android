dfa98340d4f40a1d8b5b70ce4ee89c1b95cb1a76
hongbeomi <dks3963@naver.com>
2021-10-23 22:16:28 +0900

    first commit - create project

---
+++ app/src/main/java/github/hongbeomi/jeju_road/MainActivity.kt
@@ -0,0 +1,11 @@
+package github.hongbeomi.jeju_road
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
