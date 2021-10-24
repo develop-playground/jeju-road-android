860597eac24c1e6ee9fa4fc283e56e42401bed6e
hongbeomi <dks3963@naver.com>
2021-10-25 01:21:16 +0900

    startKoin

--- app/src/main/java/github/hongbeomi/JejuRoadApp.kt
+++ app/src/main/java/github/hongbeomi/JejuRoadApp.kt
@@ -1,6 +1,10 @@
 package github.hongbeomi
 
 import android.app.Application
+import github.hongbeomi.jeju_road.di.dispatcherModule
+import github.hongbeomi.jeju_road.di.networkModule
+import github.hongbeomi.jeju_road.di.repositoryModule
+import github.hongbeomi.jeju_road.di.useCaseModule
 import org.koin.core.context.startKoin
 
 class JejuRoadApp: Application() {
@@ -8,7 +12,12 @@ class JejuRoadApp: Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
-
+            listOf(
+                dispatcherModule,
+                networkModule,
+                repositoryModule,
+                useCaseModule
+            )
         }
     }
 
