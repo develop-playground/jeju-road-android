48f21c9ec1dd2fb902c867c676fe9dacf4ff6e0d
hongbeomi <dks3963@naver.com>
2021-11-20 21:27:52 +0900

    툴바, 리사이클러뷰 추가

---
+++ app/src/main/java/github/dev_playground/jeju_road/ui/page/RestaurantPageListAdapter.kt
@@ -0,0 +1,9 @@
+package github.dev_playground.jeju_road.ui.page
+
+import github.dev_playground.jeju_road.ui.base.BaseListAdapter
+
+//class RestaurantPageListAdapter: BaseListAdapter<>() {
+//
+//
+//
+//}
