45dacf793be5d77cbaca951a9e513fe83a5f04e2
hongbeomi <dks3963@naver.com>
2022-02-27 20:22:06 +0900

    BASE_URL 마지막에 / 붙이기

--- data/src/main/java/github/dev_playground/jeju_road/data/api/RestaurantApi.kt
+++ data/src/main/java/github/dev_playground/jeju_road/data/api/RestaurantApi.kt
@@ -7,10 +7,10 @@ import retrofit2.http.Path
 
 interface RestaurantApi {
 
-    @GET("/restaurant")
+    @GET("restaurant")
     suspend fun getRestaurantList(): RestaurantData
 
-    @GET("/restaurant/{id}")
+    @GET("restaurant/{id}")
     suspend fun getRestaurantDetail(@Path("id") id: String): RestaurantDetailData
 
 }
