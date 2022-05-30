42084cb4ebef2bbdc4587895ed240b752bcc051a
Junhyung Cho <junhyung@edu.hanbat.ac.kr>
2022-05-30 19:54:41 +0900

    RestaurantListViewModelTest, RestaurantDetailViewModelTest Unit Test Code     - 테스트 코드 안에 불필요한 toUiState 컨버팅 삭제     - whenever 동기화 이슈 해결     - loadContentList의 result 값이 에러를 반환할 때 테스트를 하는 것이 옳은 것인가에 대한 의문

--- app/src/test/java/github/dev_playground/jeju_road/presentation/viewmodel/RestaurantListViewModelTest.kt
+++ app/src/test/java/github/dev_playground/jeju_road/presentation/viewmodel/RestaurantListViewModelTest.kt
@@ -6,33 +6,30 @@ import github.dev_playground.jeju_road.domain.usecase.GetRestaurantListUseCase
 import github.dev_playground.jeju_road.presentation.ui.list.RestaurantListViewModel
 import github.dev_playground.jeju_road.presentation.util.UiState
 import github.dev_playground.jeju_road.presentation.util.getOrAwaitValue
-import github.dev_playground.jeju_road.presentation.util.toUiState
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
+import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
 import org.mockito.kotlin.mock
-import org.junit.Assert.assertEquals
-import org.mockito.kotlin.notNull
 import org.mockito.kotlin.whenever
+import java.lang.IllegalStateException
 
 @ExperimentalCoroutinesApi
 class RestaurantListViewModelTest : BaseTest() {
     private lateinit var restaurantListViewModel: RestaurantListViewModel
     private val getRestaurantListUseCase: GetRestaurantListUseCase = mock()
     private val pageIndex = 0
-    private val uiStateList = UiState(
-        loading = false,
-        exception = null,
-        listOf(
-            Content(
-                id = 1,
-                name = "맛집",
-                categories = listOf("category"),
-                address = "한밭대학교",
-                image = "대충 이미지 URL",
-                introduction = "대충 소개글"
-            )
+    private val contentList = listOf(
+        Content(
+            id = 1,
+            name = "맛집",
+            categories = listOf("category"),
+            address = "한밭대학교",
+            image = "대충 이미지 URL",
+            introduction = "대충 소개글"
         )
     )
 
@@ -42,14 +39,12 @@ class RestaurantListViewModelTest : BaseTest() {
     }
 
     @Test
-    fun `리스트 새로고침 성공`() = runBlocking {
-        whenever(getRestaurantListUseCase.invoke(pageIndex).toUiState())
-            .thenReturn(uiStateList)
-
-        val result = getRestaurantListUseCase.invoke(pageIndex).toUiState()
+    fun `리스트 새로고침 성공`() = runBlockingTest {
+        whenever(getRestaurantListUseCase.invoke(pageIndex))
+            .thenReturn(Result.success(contentList))
 
         //when
-        coroutineRule.pauseDispatcher()
+        coroutineRule.testDispatcher.pauseDispatcher()
         restaurantListViewModel.refreshContentList()
 
         //then
@@ -59,25 +54,24 @@ class RestaurantListViewModelTest : BaseTest() {
         )
 
         assertEquals(
-            restaurantListViewModel.recyclerState.getOrAwaitValue(),
-            null
+            restaurantListViewModel.contentListState.getOrAwaitValue().loading,
+            true
         )
-
-        coroutineRule.resumeDispatcher()
 
         assertEquals(
             restaurantListViewModel.contentListState.getOrAwaitValue(),
-            result
+            UiState(data = contentList)
         )
     }
 
     @Test
-    fun `리스트 새로고침 실패`() {
+    fun `리스트 새로고침 실패`() = runBlocking {
+        whenever(getRestaurantListUseCase.invoke(pageIndex))
+            .thenThrow(IllegalStateException("Failed to fetch information."))
+
         //when
-        coroutineRule.pauseDispatcher()
         restaurantListViewModel.refreshContentList()
 
         //then
-
     }
 }
