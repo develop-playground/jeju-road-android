1ef7b93c25a16bbd49c615116733ba9d805b33be
Junhyung Cho <junhyung@edu.hanbat.ac.kr>
2022-05-30 23:38:54 +0900

    windowManger 사용해서 splash view action bar 삭제

--- presentation/src/main/java/github/dev_playground/jeju_road/presentation/SplashActivity.kt
+++ presentation/src/main/java/github/dev_playground/jeju_road/presentation/SplashActivity.kt
@@ -1,10 +1,11 @@
 package github.dev_playground.jeju_road.presentation
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.os.Bundle
+import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import github.dev_playground.jeju_road.presentation.ui.main.MainActivity
+import github.dev_playground.jeju_road.presentation.util.startActivity
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
@@ -14,12 +15,18 @@ import kotlinx.coroutines.launch
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        )
+
         setContentView(R.layout.activity_splash)
 
         CoroutineScope(Dispatchers.IO).launch {
-            delay(3000)
-            val intent = Intent(this@SplashActivity, MainActivity::class.java)
-            startActivity(intent)
+            delay(2000)
+
+            this@SplashActivity.startActivity<MainActivity> {  }
             finish()
         }
     }
