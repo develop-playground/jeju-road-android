0f31c0ccb87773242eb621f0b46b1cabef8c5aab
Junhyung Cho <junhyung@edu.hanbat.ac.kr>
2022-07-13 21:46:32 +0900

    OpenTimeTest 테스트 검증 수정

--- app/src/test/java/github/dev_playground/jeju_road/domain/usecase/model/OpenTimeTest.kt
+++ app/src/test/java/github/dev_playground/jeju_road/domain/usecase/model/OpenTimeTest.kt
@@ -3,78 +3,30 @@ package github.dev_playground.jeju_road.domain.usecase.model
 import github.dev_playground.jeju_road.domain.model.DayKor
 import github.dev_playground.jeju_road.domain.model.OpenTime
 import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 
 class OpenTimeTest {
 
-    private val openTime = listOf(
-        OpenTime(
-            id = 1L,
-            day = DayKor.MON,
-            operationStart = "",
-            operationEnd = "",
-            breakStart = "",
-            breakEnd = "",
-        ),
-        OpenTime(
-            id = 1L,
-            day = DayKor.TUE,
-            operationStart = "",
-            operationEnd = "",
-            breakStart = "",
-            breakEnd = "",
-        ),
-        OpenTime(
-            id = 1L,
-            day = DayKor.WED,
-            operationStart = "",
-            operationEnd = "",
-            breakStart = "",
-            breakEnd = "",
-        ),
-        OpenTime(
-            id = 1L,
-            day = DayKor.THU,
-            operationStart = "",
-            operationEnd = "",
-            breakStart = "",
-            breakEnd = "",
-        ),
-        OpenTime(
-            id = 1L,
-            day = DayKor.FRI,
-            operationStart = "",
-            operationEnd = "",
-            breakStart = "",
-            breakEnd = "",
-        ),
-        OpenTime(
-            id = 1L,
-            day = DayKor.SAT,
-            operationStart = "",
-            operationEnd = "",
-            breakStart = "",
-            breakEnd = "",
-        ),
-        OpenTime(
-            id = 1L,
-            day = DayKor.SUN,
-            operationStart = "",
-            operationEnd = "",
-            breakStart = "",
-            breakEnd = "",
-        )
-    )
+    private lateinit var mockOpenTime: OpenTime
+
+    @Before
+    fun setUp() {
+        mockOpenTime = mock()
+    }
 
     @Test
     fun `현재_날짜의_요일을_제대로_계산하는지에_대한_검증_테스트`() {
+        //given
+        whenever(mockOpenTime.currentDayOfWeek())
+            .thenReturn(DayKor.values()[3])
 
-        //when
-        val isDayOfWeek = openTime.any {
-            it.day.dayOfWeek == it.currentDayOfWeek().dayOfWeek
-        }
+        val currentDayOfWeek = mockOpenTime.currentDayOfWeek().dayOfWeek
 
         //then
-        assertEquals(true, isDayOfWeek)
+        assertEquals("수", currentDayOfWeek)
     }
 }
+
